ea19392d2ef89b70669dcdafe353ea6512dfcc2d
RedSpiderMkV <redspidermkv@hotmail.com>
2016-06-10 22:59:24 +0100

    removing commented out code

--- src/retrieveIpScript.py
+++ src/retrieveIpScript.py
@@ -9,10 +9,6 @@
 # Licence:     ..
 #-------------------------------------------------------------------------------
 
-
-#import tkMessageBox
-#from IpRetrieverLib import IpRetriever
-import time
 from Mailer.SendMail import SendMail
 from Mailer.SmtpProviders import SmtpProviders
 from IpTracker.IpRetriever import IpRetriever
@@ -22,6 +18,7 @@ from IpTracker.IpComparator import IpComparator
 gmailUserName = "someone@gmail.com"
 gmailPassword = "password"
 recipient = "recipient@somewhere.com"
+provider = SmtpProviders.OUTLOOK
 
 def main():
     ipRetriever = IpRetriever()
@@ -31,28 +28,11 @@ def main():
     if ipComparator.IsIpAddressDifferent():
         print 'IP address is different'
         fileHandler.SaveIpInfo(ipComparator.GetNewIpAddress())
+
+        mailer = SendMail(gmailUserName, gmailPassword, recipient, provider)
+        mailer.Send(ipComparator.GetNewIpAddress())        
     else:
         print 'No change in IP address'
-
-#    ipAddress = ipRetriever.GetIpAddress()
-#    print 'Retrieved IP Address: ', ipAddress
-#    
-#    fileHandler = IpFileHandler()
-#    #fileHandler.SaveIpInfo(ipAddress)
-#    print 'IP Address In File: ', fileHandler.GetIpAddressFromFile()
-#    
-#    timestamp = fileHandler.GetIpUpdateDateFromFile()
-#    if timestamp == None:
-#        print 'No timestamp'
-#    else:
-#        print 'IP Address Timestamp: ', time.strftime(
-#                                        fileHandler.DateTimeFormat, timestamp)
-
-    #if ipRetriever.IpAddressChanged():
-    #    mailer = SendMail(gmailUserName, gmailPassword, recipient, SmtpProviders.GMAIL)
-    #    mailer.Send(ipRetriever.NewIp)
-
-    #    tkMessageBox.showinfo(title="Ip Change", message="IP Address changed\nNew IP: " + ipRetriever.NewIp)
 
 if __name__ == "__main__":
     main()
