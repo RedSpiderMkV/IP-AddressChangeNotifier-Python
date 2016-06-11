6983fb3bea5d7a48388fdfb3c82bb4d3a5633a3b
RedSpiderMkV <redspidermkv@hotmail.com>
2016-06-11 20:40:32 +0100

    comments added to mail sender

--- src/Mailer/SendMail.py
+++ src/Mailer/SendMail.py
@@ -13,13 +13,29 @@ import email
 import smtplib
 
 class SendMail:
+    ''' Send an email message. '''
+    
     def __init__(self, userName, password, recipient, provider):
+        ''' Instantiate new email sending object.
+        
+            Args:
+                userName: user name of account sending the email as string.
+                password: account password as string.
+                recipient: email recipient address as string.
+                provider: smtp provider as string. '''
+                
         self.recipient = recipient
         self.userName = userName
         self.passWord = password
         self._smtpProvider = provider
 
     def Send(self, subject, message):
+        ''' Send an email message with specified subject end message.
+        
+            Args:
+                subject: email subject as string.
+                message: email message as string. '''
+                
         msg = email.message_from_string(message)
         msg['From'] = self.userName
         msg['To'] = self.recipient
